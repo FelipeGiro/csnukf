dde3b82fe9d8774d2f0ef6234723ba276542ce0e
Felipe Giro <fgiro@uliege.be>
2023-09-26 09:57:37 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='Closed Skew Normal Unscented Kalman Filter',
+    url='https://github.com/FelipeGiro/csnukf',
+    author='Felipe Giro',
+    author_email='felipelac.giro@gmail.com',
+    packages=['csnukf'],
+    install_requires=['numpy'],
+    version='0.1',
+    license='MIT',
+    description='An example of a python package from pre-existing code',
+
+)
