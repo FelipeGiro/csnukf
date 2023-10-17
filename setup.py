950429762c82a34d556fb79471f7c098db957045
Felipe <fgiro@uliege.be>
2023-10-17 12:39:44 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+DESCRIPTION = "Closed skew normal unscented Kalman filter"
+LONG_DESCRIPTION = "Implementation of the algorithm mainly described in Javad Rezaie & Jo Eidsvik (2016) A skewed unscented Kalman filter, International Journal of Control, 89:12, 2572-2583, DOI: 10.1080/00207179.2016.1171912 "
 
 setup(
-    name='Closed Skew Normal Unscented Kalman Filter',
+    name='csnukf',
+    version = VERSION,
     url='https://github.com/FelipeGiro/csnukf',
     author='Felipe Giro',
     author_email='felipelac.giro@gmail.com',
-    packages=['csnukf'],
-    install_requires=['numpy'],
-    version='0.1',
+    description=DESCRIPTION,
+    long_description= LONG_DESCRIPTION,
+    packages=find_packages,
+    install_requires=['numpy', "scipy"],
     license='MIT',
-    description='An example of a python package from pre-existing code',
+    keywords=["python", "csn", "csnukf", "closed skew normal", "unscented Kalman filter"]
 )
